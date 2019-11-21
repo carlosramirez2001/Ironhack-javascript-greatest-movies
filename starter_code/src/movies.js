41d4feab439ecbc051b21a95ce556877db976758
Carlos Ramirez <20477@columbushs.com>
2019-11-21 09:09:52 -0500

    gottafinish

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,9 +2,51 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(movies){
+
+    let copy = [...movies];
+
+    copy.sort(( a, b )=>{
+        if(a.year < b.year){
+            return -1
+        }
+        else if(b.year < a.year){
+            return 1
+        }
+        else{
+            if(a.title < b.title){
+            return -1;
+            }
+        else if(b.title < a.title){
+            return 1
+        }
+        else{
+            return 0
+        }
+     }
+    })
+
+    return copy;
+}
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(movies){
+
+    let movieBySteven = movies.filter ((eachMovie) => {
+        return eachMovie.director == "Steven Spielberg" && eachMovie.genre.includes("Drama") ;
+    })
+
+    return movieBySteven.length;
+
+
+}
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(movies){
+    
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
